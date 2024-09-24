166a4f794e1b1a38e955a0c64d003d0ef634f219
lena <andrianovalena1@gmail.com>
2024-09-24 19:50:25 +0300

    Сделала анимацию и скрытие, настроила вращение

---
+++ index.js
@@ -0,0 +1,49 @@
+const progressBar = document.querySelector('.progress-bar');
+
+const inputField = document.getElementById('progress-input');
+
+const checkboxRotation = document.getElementById('rotation-toggle');
+
+const checkboxHide = document.getElementById('hide-toggle');
+
+const progressCircle = document.querySelector('.progress-circle ');
+
+const circumference = 2 * Math.PI * 90;
+
+// Функция обновления прогресса
+function updateProgress(value) {
+    const offset = circumference - (value / 100) * circumference;
+    progressBar.style.strokeDashoffset = offset;
+}
+
+// Событие при вводе значения
+inputField.addEventListener('input', (e) => {
+    let value = parseInt(e.target.value);
+    if (isNaN(value) || value < 0) {
+        value = 0;
+    } else if (value > 100) {
+        value = 100;
+    }
+    updateProgress(value);
+});
+
+// Для вращения
+checkboxRotation.addEventListener('change', function () {
+    if (this.checked) {
+        progressCircle.classList.add('rotate');
+    } else {
+        progressCircle.classList.remove('rotate');
+    }
+});
+
+// Для скрытия
+checkboxHide.addEventListener('change', function () {
+    if (this.checked) {
+        progressCircle.classList.add('hidden-block');
+    } else {
+        progressCircle.classList.remove('hidden-block');
+    }
+});
+
+// Инициализация начального прогресса
+updateProgress(0);
