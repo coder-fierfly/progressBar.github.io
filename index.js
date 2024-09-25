014e37c10a2d7cc648bc2cfa6566155fffc04107
lena <andrianovalena1@gmail.com>
2024-09-25 16:12:39 +0300

    сделала проверку на пустую строку

--- index.js
+++ index.js
@@ -17,10 +17,23 @@ function updateProgress(value) {
 
 // Событие при вводе значения
 inputField.addEventListener('input', (e) => {
+    console.log(parseFloat(e.target.value))
+    console.log("!!")
     let value = parseFloat(e.target.value);
+
+    if (e.target.value.trim() === "") {
+        if (checkboxRotation.checked) {
+            checkboxRotation.checked = false;
+            progressCircle.classList.remove('rotate');
+        }
+        updateProgress(0);
+        return;
+    }
+
     if (value > 100 || value < 0) {
         alert("Пожалуйста, введите число от 0 до 100.");
         inputField.value = "";
+        updateProgress(0);
         return;
     }
     updateProgress(value);
